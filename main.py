8aba9082a99c9dd76867e60e9260f31c4ead1077
JonJarrett <jonathan.jarrett86@gmail.com>
2024-10-09 17:04:49 -0700

    Ver 1.0

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
 import tkinter
 from tkinter import messagebox
+from tkinter import ttk
 from datetime import datetime
 import sqlite3
+
+
+def add_item_table():
+    date = view_vitals_date_selection.get()
+    conn = sqlite3.connect('data.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM vitals_db WHERE date = '" + date + "'")
+    data = cursor.fetchall()
+    for item in data:
+        table.insert('', 'end', text=item[0], values=item[1:])
+    conn.close()
+
+
+def add_multi_item_table():
+    date = view_vitals_date_selection.get()
+    date_range = range_date_selection.get()
+    conn = sqlite3.connect('data.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM vitals_db WHERE date BETWEEN '" + date + "' and '" + date_range +
+                   "' ORDER by date")
+    data = cursor.fetchall()
+    for item in data:
+        table.insert('', 'end', text=item[0], values=item[1:])
+    conn.close()
+
+
+def clear_table():
+    for item in table.get_children():
+        table.delete(item)
 
 
 def submit_data():
@@ -126,14 +156,26 @@ def update_data():
 if __name__ == '__main__':
     window = tkinter.Tk()
     window.title('Daily Vital Tracking')
-    window.geometry('600x400')
-    frame = tkinter.Frame(window)
-    frame.pack()
+    window.geometry('600x600')
 
     current_date = datetime.today().strftime('%m/%d/%Y')
 
-    vitals_frame = tkinter.LabelFrame(frame)
-    vitals_frame.grid(rows=5, column=3)
+    notebook = ttk.Notebook(window)
+
+    input_tab = ttk.Frame(notebook)
+    input_tab_label = tkinter.Label(input_tab)
+    input_tab_label.pack()
+
+    view_vitals_tab = ttk.Frame(notebook)
+    view_vitals_tab_label = tkinter.Label(view_vitals_tab)
+    view_vitals_tab_label.pack()
+
+    notebook.add(input_tab, text='Input Vitals')
+    notebook.add(view_vitals_tab, text='View Vitals')
+    notebook.pack()
+
+    vitals_frame = tkinter.LabelFrame(input_tab)
+    vitals_frame.pack()
 
     top_banner = tkinter.Label(vitals_frame, text='Please enter your Vitals for today')
     top_banner.grid(row=1, column=3, pady=10)
@@ -175,12 +217,54 @@ if __name__ == '__main__':
     bp_entry.grid(row=9, column=4, padx=5, pady=(0, 5))
     bp_entry.insert(0, '0')
 
-    submit = tkinter.Button(window, text='SUBMIT', bg='#a9a9a9', command=submit_data)
+    submit = tkinter.Button(vitals_frame, text='SUBMIT', bg='#a9a9a9', command=submit_data)
     submit.config(font=48)
-    submit.pack(pady=30)
+    submit.grid(row=11, column=3, pady=(25, 0))
 
-    update = tkinter.Button(window, text='UPDATE', bg='#a9a9a9', command=update_data)
+    update = tkinter.Button(vitals_frame, text='UPDATE', bg='#a9a9a9', command=update_data)
     update.config(font=48)
-    update.pack()
+    update.grid(row=13, column=3, pady=10)
+
+    view_vitals_frame = tkinter.LabelFrame(view_vitals_tab)
+    view_vitals_frame.pack()
+
+    view_vitals_select_label = tkinter.Label(view_vitals_frame, text='Single date')
+    view_vitals_select_label.grid(row=1, column=0,)
+    view_vitals_date_selection = tkinter.Entry(view_vitals_frame, width=10)
+    view_vitals_date_selection.grid(row=2, column=0, pady=(10, 0))
+    view_vitals_date_selection.insert(0, current_date)
+
+    range_select_label = tkinter.Label(view_vitals_frame, text='Date range')
+    range_select_label.grid(row=1, column=1, )
+    range_date_selection = tkinter.Entry(view_vitals_frame, width=10)
+    range_date_selection.grid(row=2, column=1, pady=(10, 0))
+    range_date_selection.insert(0, current_date)
+
+    view_vitals_label_button = tkinter.Button(view_vitals_frame, text='Single Date', bg='#a9a9a9',
+                                              command=add_item_table)
+    view_vitals_label_button.grid(row=3, column=0, padx=(10, 0), pady=10)
+
+    range_view_vitals_label_button = tkinter.Button(view_vitals_frame, text='Multiple Dates', bg='#a9a9a9',
+                                                    command=add_multi_item_table)
+    range_view_vitals_label_button.grid(row=3, column=1, padx=10, pady=10)
+
+    clear_table_button = tkinter.Button(view_vitals_frame, text='CLEAR', bg='#a9a9a9', command=clear_table)
+    clear_table_button.grid(row=3, column=2, padx=(0, 10))
+
+    table = ttk.Treeview(view_vitals_tab_label, height=20)
+    table['columns'] = ('Weight', 'Fat %', 'Sugar', 'Pulse', 'Blood Pressure')
+    table.column('#0', anchor='center', width=85)
+    table.heading('#0', anchor='center', text='Date')
+    table.column('#1', anchor='center', width=50)
+    table.heading('#1', anchor='center', text='Weight')
+    table.column('#2', anchor='center', width=50)
+    table.heading('#2', anchor='center', text='Fat %')
+    table.column('#3', anchor='center', width=50)
+    table.heading('#3', anchor='center', text='Sugar')
+    table.column('#4', anchor='center', width=50)
+    table.heading('#4', anchor='center', text='Pulse')
+    table.column('#5', anchor='center', width=100)
+    table.heading('#5', anchor='center', text='Blood Pressure')
+    table.pack()
 
     window.mainloop()
